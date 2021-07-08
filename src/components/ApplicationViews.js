7b4057e0fe95852fe6a8c7ce614537a88294a1f6
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-08 13:43:48 -0500

    added components for customers, employees and locations

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { Home } from "./Home";
 import { AnimalCard } from "./animal/AnimalCard";
+import { CustomerCard } from "./customer/Customer";
+import { EmployeeCard } from "./employee/Employee";
+import { LocationCard } from "./location/Location";
 
 export const ApplicationViews = () => {
   return (
@@ -14,6 +17,15 @@ export const ApplicationViews = () => {
       {/* Render the animal list when http://localhost:3000/animals */}
       <Route path="/animals">
         <AnimalCard />
+      </Route>
+      <Route path="/customers">
+        <CustomerCard />
+      </Route>
+      <Route path="/employees">
+        <EmployeeCard />
+      </Route>
+      <Route path="/locations">
+        <LocationCard />
       </Route>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Kennel } from "./components/Kennel";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Kennel />
+    <Router>
+      <Kennel />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
