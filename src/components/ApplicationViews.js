ebedfae1e05cbff278575042bd8976ff65f51d7a
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-08 15:11:24 -0500

    added code from syllabus into AnimalProvider/AnimalList/AnimalCard to get the database animals to show in the browser

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { Home } from "./Home";
-import { AnimalCard } from "./animal/AnimalCard";
+import { AnimalProvider } from "./animal/AnimalProvider";
+import { AnimalList } from "./animal/AnimalList";
 import { CustomerCard } from "./customer/Customer";
 import { EmployeeCard } from "./employee/Employee";
 import { LocationCard } from "./location/Location";
@@ -15,9 +16,12 @@ export const ApplicationViews = () => {
       </Route>
 
       {/* Render the animal list when http://localhost:3000/animals */}
-      <Route path="/animals">
-        <AnimalCard />
-      </Route>
+      <AnimalProvider>
+        <Route exact path="/animals">
+          <AnimalList />
+        </Route>
+      </AnimalProvider>
+
       <Route path="/customers">
         <CustomerCard />
       </Route>
