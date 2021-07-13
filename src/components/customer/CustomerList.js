cacb29191a955099aeaebac405393657bc87c9bd
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-13 08:31:44 -0500

    set up files necessary for adding the proper data to the other pages

---
+++ src/components/customer/CustomerList.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from "react";
+import { CustomerContext } from "./CustomerProvider";
+import { CustomerCard } from "./Customer";
+import "./Customer.css";
+
+export const CustomerList = () => {
+  // This state changes when `getCustomers()` is invoked below
+  const { customers, getCustomers } = useContext(CustomerContext);
+
+  //useEffect - reach out to the world for something
+  useEffect(() => {
+    console.log("CustomerList: useEffect - getCustomers");
+    getCustomers();
+  }, []);
+
+  return (
+    <div className="customers">
+      {console.log("CustomerList: Render", customers)}
+      {customers.map((customer) => {
+        return <CustomerCard key={customer.id} customer={customer} />;
+      })}
+    </div>
+  );
+};
