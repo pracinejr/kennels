969dc49788d25cde7e6eb26c0f813ad3020c936b
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-13 15:34:55 -0500

    added new employee button/functionality

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,24 +1,47 @@
 import React, { useContext, useEffect } from "react";
 import { EmployeeContext } from "./EmployeeProvider";
 import { EmployeeCard } from "./EmployeeCard";
+import { LocationContext } from "../location/LocationProvider";
+import { useHistory } from "react-router-dom";
 import "./Employee.css";
 
 export const EmployeeList = () => {
   // This state changes when `getEmployees()` is invoked below
   const { employees, getEmployees } = useContext(EmployeeContext);
+  const { locations, getLocations } = useContext(LocationContext);
+
+  const history = useHistory();
 
   //useEffect - reach out to the world for something
   useEffect(() => {
     console.log("EmployeeList: useEffect - getEmployees");
-    getEmployees();
+    getEmployees().then(getLocations);
   }, []);
 
   return (
-    <div className="employees">
-      {console.log("EmployeeList: Render", employees)}
-      {employees.map((employee) => {
-        return <EmployeeCard key={employee.id} employee={employee} />;
-      })}
-    </div>
+    <>
+      <h2>Employees</h2>
+      <button
+        onClick={() => {
+          history.push("/employees/newEmployee");
+        }}
+      >
+        Add New Employee
+      </button>
+      <div className="employees">
+        {console.log("EmployeeList: Render", employees)}
+        {employees.map((employee) => {
+          const location = locations.find((l) => l.id === employee.locationId);
+
+          return (
+            <EmployeeCard
+              key={employee.id}
+              employee={employee}
+              location={location}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
