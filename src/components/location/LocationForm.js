1e957e982df5ae06f7b6d29bf34b227998264d85
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-14 13:09:04 -0500

    added ability to add a new location

--- src/components/location/LocationForm.js
+++ src/components/location/LocationForm.js
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { LocationContext } from "./LocationProvider";
 import { useHistory } from "react-router-dom";
 import "./Location.css";
 
-export const newLocation = () => {
+export const LocationForm = () => {
   const { addLocation } = useContext(LocationContext);
-  const { locations, getLocations } = useContext(LocationContext);
+  // const { locations, getLocations } = useContext(LocationContext);
 
   const [location, setLocation] = useState({
     name: "",
@@ -14,9 +14,9 @@ export const newLocation = () => {
 
   const history = useHistory();
 
-  useEffect(() => {
-    getLocations();
-  }, []);
+  // useEffect(() => {
+  //   getLocations();
+  // }, []);
 
   const handleControlledInputChange = (event) => {
     const newLocation = { ...location };
@@ -28,5 +28,54 @@ export const newLocation = () => {
     event.preventDefault();
 
     const locationId = parseInt(location.locationId);
+
+    if (locationId === 0) {
+      window.alert("Please select a location");
+    } else {
+      const newLocation = {
+        name: location.name,
+        address: location.address,
+      };
+      addLocation(newLocation).then(() => history.push("/locations"));
+    }
   };
+
+  return (
+    <form className="locationForm">
+      <h2 className="locationForm__title">New Location</h2>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">Location name:</label>
+          <input
+            type="text"
+            id="name"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Location name"
+            value={location.name}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">Location address:</label>
+          <input
+            type="text"
+            id="address"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Location address"
+            value={location.address}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <button className="btn btn-primary" onClick={handleClickSaveLocation}>
+        Save Location
+      </button>
+    </form>
+  );
 };
--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -2,11 +2,12 @@ import React, { useContext, useEffect } from "react";
 import { LocationContext } from "./LocationProvider";
 import { LocationCard } from "./LocationCard";
 import "./Location.css";
+import { useHistory } from "react-router-dom";
 
 export const LocationList = () => {
   // This state changes when `getLocations()` is invoked below
   const { locations, getLocations } = useContext(LocationContext);
-
+  const history = useHistory();
   //useEffect - reach out to the world for something
   useEffect(() => {
     console.log("LocationList: useEffect - getLocations");
@@ -14,11 +15,21 @@ export const LocationList = () => {
   }, []);
 
   return (
-    <div className="locations">
-      {console.log("LocationList: Render", locations)}
-      {locations.map((location) => {
-        return <LocationCard key={location.id} location={location} />;
-      })}
-    </div>
+    <>
+      <h2>Locations</h2>
+      <button
+        onClick={() => {
+          history.push("/locations/newLocations");
+        }}
+      >
+        Add New Location
+      </button>
+      <div className="locations">
+        {console.log("LocationList: Render", locations)}
+        {locations.map((location) => {
+          return <LocationCard key={location.id} location={location} />;
+        })}
+      </div>
+    </>
   );
 };
